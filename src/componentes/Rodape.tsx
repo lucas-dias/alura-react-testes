e0d3c3b96ab5af3faf7b6212f82bb2ce206988e5
Lucas Dias <lf-dias@outlook.com>
2023-07-27 15:39:13 -0300

    bug fix

--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { useListaParticipantes } from "../state/hooks/useListaParticipantes"
 import { useSorteador } from "../state/hooks/useSorteador"
-import sacolas from "../imagens/sacolas.png"
 import estilos from "./Rodape.module.scss"
 
 const Rodape = () => {
@@ -20,7 +19,7 @@ const Rodape = () => {
   return (
     <footer className={estilos.rodape}>
       <button disabled={participantes.length < 3} onClick={iniciar} className={estilos.botao}>Sortear</button>
-      <img src={sacolas} alt="Sacolas de compra" />
+      <img src="/imagens/sacolas.png" alt="Sacolas de presente" />
     </footer>
   )
 }
